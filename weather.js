450179e243f323122238c938cc1c249029b59613
ghostlyphobic <novaacat@gmail.com>
2024-06-29 11:54:47 -0400

    changes to weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,20 @@
-/* javascript*/ 
+let inputLocation = document.getElementsByClassName("location-input");
+let weatherImage = document.getElementsByClassName("weather-icon");
+
+let now = newdate;
+let time = now.twoLocalTimeString;
+
+let days = [
+    
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wendsday',  
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+let day = days[now.getDay()];
+
+
